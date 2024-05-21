dda43133d5956aea4468b952c11b00d9eabec9d7
Sebastian Arredondo <sarredondoloa@cesde.net>
2024-05-21 18:11:54 -0500

    TallerDom-Nodos
    Entrega de taller

---
+++ TallerDOM-Nodos/JS NODOS/nodos.js
@@ -0,0 +1,73 @@
+//Ejercicio #1
+
+// let titulo = document.createElement("h1")
+// titulo.textContent = "my presentation"
+// let ubiTitulo = document.querySelector("div")
+// ubiTitulo.insertAdjacentElement("beforebegin",titulo)
+
+//Ejercicio #2
+
+// let texto1 = document.createElement("p")
+// texto1.textContent = "my best friend is mickey"
+// let ubiTexto = document.querySelector("p")
+// ubiTexto.insertAdjacentElement("afterend",texto1)
+
+//Ejercicio #3
+
+// let titulo = document.querySelector("h1");
+// titulo.style.color = "green";
+
+//Ejercicio #4 por revisar 
+
+//    // Seleccionamos la etiqueta <ol> por su ID
+//    const ol = document.getElementById('miLista');
+
+//    // Creamos una nueva etiqueta <ul>
+//    const ul = document.createElement('ul');
+//    ul.className = 'listado'; // Agregamos el atributo class con el nombre listado
+
+//    // Movemos todos los elementos <li> del <ol> al <ul>
+//    while (ol.firstChild) {
+//        ul.appendChild(ol.firstChild);
+//    }
+
+//    // Reemplazamos el <ol> por el nuevo <ul>
+//    ol.replaceWith(ul);
+
+
+// Ejercicio #5
+
+// let subtitulo = document.querySelector("h2")
+// subtitulo.classList.add("estilo")
+
+// Ejercicio #6
+
+// let selec = document.querySelectorAll("ol li")
+// let agregar = document.createElement("li")
+
+// agregar.textContent= "Recoger a los niños"
+// selec[3].insertAdjacentElement("beforeend",agregar)
+
+// selec[0].classList.add("nomCumplido")
+// selec[1].classList.add("nomFallido")
+// selec[2].classList.add("nomFallido")
+// selec[3].classList.add("nomCumplido")
+// selec[4].classList.add("nomCumplido")
+
+// // Ejercicio #7
+
+// let estiloP = document.querySelectorAll("p.ps")
+
+// estiloP.forEach( estiloP => {
+//     estiloP.style.color= "blue";
+//     estiloP.style.fontSize= "20px";
+// })
+
+
+
+
+
+
+
+
+
